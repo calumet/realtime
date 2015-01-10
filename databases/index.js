b1aa4a435b9dcb96892424206079f64eec17d075
Romel Pérez <ronelprhone@gmail.com>
2015-01-10 16:38:35 -0500

    complete: inicialización de bases de datos

--- databases/index.js
+++ databases/index.js
@@ -1,23 +1,52 @@
 /*!
+ * Universidad Industrial de Santander
  * Grupo de Desarrollo de Software Calumet
  * Realtime | Databases
  * Romel Pérez, prhone.blogspot.com
- * 2014
+ * 2015
  **/
 
+var rubi = require('../libs/rubi');
+var diamante = require('../libs/diamante');
 var portal = require('./dbs.portal');
-//var aula = require('./dbs.aula');
 var debug = require('debug')('dbs');
 
 
 // -------------------------------------------------------------------------- //
-// GLOBAL DATABASE PROCEDURES //
+// GLOBAL DATABASES PROCEDURES //
 
-module.exports = exports = function () {
+/**
+ * Se ejecutan los procedimientos a realizarse en las bases de datos una vez
+ * se inicie el servidor.
+ *
+ * > Recordar que una vez se inicie la conexión con el servidor, las demás
+ * instancias de libs/rubi y libs/diamante podrán hacer todos los procedimientos
+ * que necesiten.
+ *
+ * > Se saldrá el servidor sino se pudo conectar con alguna de las bases de datos.
+ */
 
-    // Resetear todas las instancias de los usuarios.
-    // Si habían conectados, se reconectarán y se recrean otra vez tales instancias.
-    portal.reset(function (err) {
-        if (err) debug(err);
-    });
+module.exports = exports = function (callback) {
+
+	// Conectarse con rubi.
+	rubi.connect(function (err) {
+		if (err) throw err;
+		else debug('rubi está conectada.');
+
+		// Conectarse con diamante.
+		diamante.connect(function (err) {
+			if (err) throw err;
+			else debug('diamante está conectada.');
+
+		  // Resetear todas las instancias de los usuarios. Si habían conectados,
+		  // se reconectarán y se recrean otra vez tales instancias.
+	    portal.reset(function (err) {
+	      if (err) debug(err);
+	    });
+
+		  // Iniciar todas las labores que necesiten de las bases de datos.
+			callback();
+		});
+	});
+
 };
