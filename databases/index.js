2cd0397081daf031f1fa491b65bb3d39a3220a07
Romel Pérez <ronelprhone@gmail.com>
2015-01-12 20:50:54 -0500

    add: agregando modo de resetear instancias en ac_rooms

--- databases/index.js
+++ databases/index.js
@@ -9,6 +9,8 @@
 var rubi = require('../libs/rubi');
 var diamante = require('../libs/diamante');
 var portal = require('./dbs.portal');
+var aula = require('./dbs.aula');
+var async = require('async');
 var debug = require('debug')('dbs');
 
 
@@ -18,35 +20,45 @@ var debug = require('debug')('dbs');
 /**
  * Se ejecutan los procedimientos a realizarse en las bases de datos una vez
  * se inicie el servidor.
+ * @param {Function} callback Los procesos iniciales se han cargado.
  *
  * > Recordar que una vez se inicie la conexión con el servidor, las demás
  * instancias de libs/rubi y libs/diamante podrán hacer todos los procedimientos
  * que necesiten.
  *
  * > Se saldrá el servidor sino se pudo conectar con alguna de las bases de datos.
+ *
+ * > Las instancias de conexiones con los sockets se reestablecen una vez
+ * ĺa conexión del server se reanuda.
  */
-
 module.exports = exports = function (callback) {
 
-	// Conectarse con rubi.
-	rubi.connect(function (err) {
-		if (err) throw err;
-		else debug('rubi está conectada.');
+  // Conectarse con rubi.
+  rubi.connect(function (err) {
+    if (err) throw err;
+    else debug('rubi está conectada.');
 
-		// Conectarse con diamante.
-		diamante.connect(function (err) {
-			if (err) throw err;
-			else debug('diamante está conectada.');
+    // Conectarse con diamante.
+    diamante.connect(function (err) {
+      if (err) throw err;
+      else debug('diamante está conectada.');
 
-		  // Resetear todas las instancias de los usuarios. Si habían conectados,
-		  // se reconectarán y se recrean otra vez tales instancias.
-	    portal.reset(function (err) {
-	      if (err) debug(err);
-	    });
+      // Procesos iniciales.
+      async.parallel([
 
-		  // Iniciar todas las labores que necesiten de las bases de datos.
-			callback();
-		});
-	});
+        // Resetear todas las instancias de los usuarios.
+        portal.reset,
+
+        // Resetear todas las instancias de usuarios conectados en rubi.ac_rooms.
+        aula.reset
+
+      ], function (err) {
+        if (err) debug(err);
+
+        // Iniciar todas las labores que necesiten de las bases de datos.
+        callback();
+      });
+    });
+  });
 
 };
